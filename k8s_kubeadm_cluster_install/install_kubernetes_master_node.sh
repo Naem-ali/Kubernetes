4e9ec657119cb17063c30fec3d41ee79480a1370
Naeem <naeem.ali@gmx.de>
2022-01-04 22:06:52 +0100

    cluster health check and report objects

--- k8s_kubeadm_cluster_install/install_kubernetes_master_node.sh
+++ k8s_kubeadm_cluster_install/install_kubernetes_master_node.sh
@@ -1,7 +1,6 @@
 #!/bin/bash
 
 ###### install first docker incase you don't have it installed #######
-
  echo "installing docker"
 apt-get update
 apt-get install -y \
@@ -14,18 +13,13 @@ add-apt-repository \
    "deb https://download.docker.com/linux/$(. /etc/os-release; echo "$ID") \
    $(lsb_release -cs) \
    stable"
-
 apt-get update
 apt-get install docker-ce docker-ce-cli containerd.io -y
-
 sleep 3
-
 docker -version
 docker ps 
-
 #### now you can start installing kubernetes #####
 ### Requirement -> 2G memory, atleast 2 CPUs -> Linux: ubuntu: 20.4.1 LTS #####
-
 echo "installing kubernetes_Kubeadm_cluster"
 apt-get update
 sudo apt install curl
@@ -33,12 +27,10 @@ apt-get install -y apt-transport-https
 curl -s https://packages.cloud.google.com/apt/doc/apt-key.gpg | apt-key add -
 cat <<EOF >/etc/apt/sources.list.d/kubernetes.list
 deb http://apt.kubernetes.io/ kubernetes-xenial main
-EOF
-apt-get update
+EOF  
 apt-get install -y kubelet kubeadm kubectl
-
-### create an actual cluster ###
-kubeadm init --pod-network-cidr=192.168.0.0/16  # you can change the pod network cidr
+### create an actual cluster, you can change the pod network cidr ###
+kubeadm init --pod-network-cidr=192.168.0.0/16 
 # this will take some time and after that the following instructions will be on your screen, please save the token from the screen in a file 
 # you will need it when adding nodes to the Master, starting with kubeadm join --token
 
